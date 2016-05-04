e7f787c6569c59a2e157f2944ea5799fc8f60d59
Sam Milechman <sammilechman@gmail.com>
2016-05-03 20:44:16 -0700

    add basic questionnaire functionality

---
+++ app/controllers/questionnaires_controller.rb
@@ -0,0 +1,25 @@
+class QuestionnairesController < ApplicationController
+  def new
+    @questionnaire = current_user.questionnaires.new
+    current_user.habits.each do |habit|
+      @questionnaire.questions.new(habit_id: habit.id)
+    end
+  end
+
+  def create
+    @questionnaire = current_user.questionnaires.new(questionnaire_params)
+    if @questionnaire.save
+      flash[:notice] = 'Success!'
+      redirect_to root_url
+    else
+      flash[:alert] = 'Error'
+      redirect_to new_questionnaire_url
+    end
+  end
+
+  private
+
+  def questionnaire_params
+    params.require(:questionnaire).permit(questions_attributes: [:answer])
+  end
+end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,4 +1,5 @@
 class Questionnaire < ActiveRecord::Base
   belongs_to :user
   has_many :questions
+  accepts_nested_attributes_for :questions
 end
